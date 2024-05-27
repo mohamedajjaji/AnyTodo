ec58d8ee3924e8fe96a5565489fc0cdbec1d31de
Mohamed AJJAJI <majjaji98@gmail.com>
2024-05-27 16:39:03 +0000

    Fix adding new task

--- backend/tasks/serializers.py
+++ backend/tasks/serializers.py
@@ -32,3 +32,9 @@ class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
         fields = '__all__'
+        read_only_fields = ['user']
+
+    def create(self, validated_data):
+        user = self.context['request'].user
+        validated_data['user'] = user
+        return super().create(validated_data)
